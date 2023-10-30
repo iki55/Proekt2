92f5afd30336a42b35765901cc13e0be817d6835
iki55 <147849800+iki55@users.noreply.github.com>
2023-10-30 22:30:02 +0100

    Add files via upload

---
+++ Smetka.java
@@ -0,0 +1,14 @@
+package proekt_1;
+
+public class Smetka {
+
+	public static void main(String[] args) {
+int j=32;
+int k=20;
+int p=50;
+double smetka=(j*2)+(k*5)+(p*2.5);
+System.out.println("smetkata iznesuva");
+System.out.println(smetka);
+
+	}
+}
